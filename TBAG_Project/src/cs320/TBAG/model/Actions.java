92d40561c459616e3ab4309df70eb7cbec62c70a
nbussiere <nbussiere@ycp.edu>
2022-03-11 15:20:59 -0500

    Test of commit with basic updates to Map and Actions class created

---
+++ TBAG_Project/src/cs320/TBAG/model/Actions.java
@@ -0,0 +1,16 @@
+package cs320.TBAG.model;
+
+public class Actions {
+	public String direction;
+	public String item;
+	
+	public void moveActor(String currActor, String direction) {
+		//Attempt to move actor 
+	}
+	
+	public void pickUpItem(String currActor, String item) {
+		//Attempt to pickup item
+	}
+	
+	
+}
--- TBAG_Project/src/cs320/TBAG/model/Map.java
+++ TBAG_Project/src/cs320/TBAG/model/Map.java
@@ -1,1 +1,28 @@
 package cs320.TBAG.model;
+
+public class Map{
+	private int roomNode;
+	private int actorCurrRoom;
+	private int actorNewRoom;
+	private boolean validMove;
+	private String enteredMove;
+	
+	public int getCurrRoom() {
+		return actorCurrRoom;
+	}
+		
+	public boolean checkMove()
+	{
+		return validMove;
+	}
+	
+	public void setNewRoom(int actorNewRoom) {
+		this.actorNewRoom = actorNewRoom;
+	}
+	
+	public void attemptMove(int roomNode, String enteredMove) {
+		//check move in CSV
+	}
+	
+	
+}
