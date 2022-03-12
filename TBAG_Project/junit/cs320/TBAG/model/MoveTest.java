93fd936d159864420d1b3bb1a0031345b715f37a
nbussiere <nbussiere@ycp.edu>
2022-03-12 16:29:47 -0500

    Added Unit Test framework for PickUp and Move

---
+++ TBAG_Project/junit/cs320/TBAG/model/MoveTest.java
@@ -0,0 +1,27 @@
+package cs320.TBAG.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+class MoveTest {
+	
+	@Before
+	void setUp() {
+		Map map = new Map();
+		
+		
+	}
+	
+	@Test
+	void testSuccesfulMove() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testIllegalMove() {
+		fail("Not yet implemented");
+	}
+
+}
--- TBAG_Project/src/cs320/TBAG/model/Actions.java
+++ TBAG_Project/src/cs320/TBAG/model/Actions.java
@@ -1,12 +1,25 @@
 package cs320.TBAG.model;
-
+import cs320.TBAG.model.Inventory;
+import cs320.TBAG.model.Map;
 
 public class Actions {
 	public String direction;
 	public String item;
-	
+			
 	public void moveActor(String currActor, String direction) {
-		//Attempt to move actor 
+		String actor = currActor;
+		String dir = direction;
+		
+		int currloc = actor.getCurrRoom();
+		if (dir.checkMove() == true) {
+			actor.setNewRoom();
+			System.out.println(roomDescrip());
+		}
+		else {
+			System.out.println("There's no way to go that diretion");
+		}
+		
+		
 	}
 	
 	public void pickUpItem(String currActor, String item) {
