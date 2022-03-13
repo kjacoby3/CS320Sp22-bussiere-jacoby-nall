3cf3bfd2c232d20438a6f1c8d5dc1ee1d8fb77ff
nbussiere <nbussiere@ycp.edu>
2022-03-13 13:49:29 -0400

    Added loadLevel() to Map

--- TBAG_Project/src/cs320/TBAG/model/Map.java
+++ TBAG_Project/src/cs320/TBAG/model/Map.java
@@ -8,6 +8,9 @@ public class Map{
 	private String enteredMove;
 	int newRoomID;
 	
+	String[][] mapLayout;
+	String[][] mapDescrips;
+	
 	
 	String[][] trialMap = {{"RoomID","RoomName","Move","ID","Move","ID"},{"1","Starting Area","north","2","n","2" },{"2","North Area","south","2","s","2","east","4","e","4"},{"3","West Area","east","1","e","1","south","5","s","5"},{"4","North East Area","west","2","w","2"},{"5","West South","north","3","n","3"}};
 	String[][] trialMapDescrip = {{"RoomID","LongDescrip"},{"1","You find yourself in the starting area.  There is a banana on the floor"},{"2","You head North into another area"},{"3","You head West into an area that seems more Western"},{"4","You head East from the North area. "},{"5","You head South from the West area"}};
@@ -61,4 +64,8 @@ public class Map{
 		return trialMapDescrip[currRoom][1];
 	}
 	
+	public void loadLevel(String[][] layout, String[][] descrip) {
+		this.mapLayout = layout;
+		this.mapDescrips = descrip;
+	}
 }
