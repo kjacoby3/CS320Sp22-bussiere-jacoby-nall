ab98df559547f782e4caec6cd84e106c88ceeac8
kjacoby3 <kjacoby3@ycp.edu>
2022-03-14 03:10:12 -0400

    Game Model Tests

--- TBAG_Project/junit/cs320/TBAG/model/GameModelTest.java
+++ TBAG_Project/junit/cs320/TBAG/model/GameModelTest.java
@@ -1,1 +1,55 @@
 package cs320.TBAG.model;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+import junit.framework.TestCase;
+
+public class GameModelTest extends TestCase{
+	Game model;
+	String username;
+	String password;
+	Save save;
+	Map map;
+	ArrayList<Actor> actorsList;
+	ArrayList<Inventory> inventories;
+	Player player1;
+	
+	@Before
+	public void setUp() {
+		model = new Game();
+		username = "kjacoby3";
+		password = "kjacoby3";
+		actorsList = new ArrayList<Actor>();
+		inventories = new ArrayList<Inventory>();
+		map = new Map();
+		save = new Save(actorsList, inventories, map);
+	}
+	
+	@Test
+	void testGetUsername() {
+		model.setUsername(username);
+		assertEquals(model.getUsername(), "kjacoby3");
+	}
+	
+	@Test
+	void testGetPassword() {
+		model.setPassword(password);
+		assertEquals(model.getPassword(), "kjacoby3");
+	}
+	
+	@Test 
+	void testGetActorsList() {
+		model.newActorsList();
+		assertEquals(model.getActorsList(), actorsList);
+	}
+
+	@Test
+	void testGetInventoriesList() {
+		model.newInventoryList();
+		assertEquals(model.getInventories(), inventories);
+	}
+}
--- TBAG_Project/src/cs320/TBAG/controller/GameController.java
+++ TBAG_Project/src/cs320/TBAG/controller/GameController.java
@@ -21,6 +21,7 @@ public class GameController {
 	}
 
 	public void newGame() {
+		model.addEmptyPlayer();
 		model.addEmptyNPC();
 	}
 	
