2810196054be43136f8e52a4ab0c5933c594a370
nbussiere <nbussiere@ycp.edu>
2022-03-14 01:06:54 -0400

    Updated Actions with inputs

--- TBAG_Project/src/cs320/TBAG/model/Actions.java
+++ TBAG_Project/src/cs320/TBAG/model/Actions.java
@@ -5,8 +5,8 @@ import cs320.TBAG.model.Item;
 
 interface ActionsInterface{
 	
-	void move();
-	void pickUp();
+	void move(String actor, String direction);
+	void pickUp(String actor, Item item);
 	void attack();
 }
 
@@ -16,7 +16,7 @@ public class Actions implements ActionsInterface {
 	Weapon banana = new Weapon("banana", 1000000000, 1000000000);
 	
 	@Override
-	public void move() {
+	public void move(String currActor, String direction) {
 		Map tempMap = new Map();
 		boolean verify = tempMap.checkMove(tempMap.getCurrRoom(), direction);
 		if(verify = true) {
@@ -31,7 +31,7 @@ public class Actions implements ActionsInterface {
 		
 	}
 	@Override
-	public void pickUp() {
+	public void pickUp(String currActor, Item item) {
 		Inventory tempInv = new Inventory(1);
 		boolean check = tempInv.addItem(banana);
 		if(check = true) {
