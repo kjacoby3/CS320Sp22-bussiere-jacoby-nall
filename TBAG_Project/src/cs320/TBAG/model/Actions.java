6236670648127dfaea1c023863eacf8098bbc800
nbussiere <nbussiere@ycp.edu>
2022-03-13 23:39:24 -0400

    Modified Actions

--- TBAG_Project/src/cs320/TBAG/model/Actions.java
+++ TBAG_Project/src/cs320/TBAG/model/Actions.java
@@ -1,25 +1,28 @@
 package cs320.TBAG.model;
 import cs320.TBAG.model.Inventory;
 import cs320.TBAG.model.Map;
+import cs320.TBAG.model.Item;
 
 interface ActionsInterface{
 	
 	void move();
 	void pickUp();
+	void attack();
 }
 
 public class Actions implements ActionsInterface {
 	public String direction;
 	public Item item;
+	Weapon banana = new Weapon("banana", 1000000000, 1000000000);
 	
 	@Override
 	public void move() {
 		Map tempMap = new Map();
-		boolean verify = tempMap.checkMove(actorCurrRoom, direction);
+		boolean verify = tempMap.checkMove(tempMap.getCurrRoom(), direction);
 		if(verify = true) {
 			int newID = tempMap.getNewRoomID();
 			tempMap.setNewRoom(newID);
-			System.out.println(tempMap.getRoomDescription(actorCurrRoom));
+			System.out.println(tempMap.getRoomDescription(tempMap.getCurrRoom()));
 		}
 		else {
 			System.out.println("There's no way to go that direction");
@@ -30,9 +33,9 @@ public class Actions implements ActionsInterface {
 	@Override
 	public void pickUp() {
 		Inventory tempInv = new Inventory(1);
-		boolean check = tempInv.addItem(item);
+		boolean check = tempInv.addItem(banana);
 		if(check = true) {
-			System.out.println("You put the " + item + " in your inventory");
+			System.out.println("You put the " + banana.getName() + " in your inventory");
 		}
 		else {
 			System.out.println("You can't add that to your inventory at this time");
