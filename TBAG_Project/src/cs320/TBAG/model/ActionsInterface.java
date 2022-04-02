8a4e0c147d794e4d4bd6f46254598d91ded3b912
nbussiere <nbussiere@ycp.edu>
2022-04-02 15:59:21 -0400

    UnEquip Changes

--- TBAG_Project/src/cs320/TBAG/model/ActionsInterface.java
+++ TBAG_Project/src/cs320/TBAG/model/ActionsInterface.java
@@ -5,7 +5,7 @@ public interface ActionsInterface {
 	void pickUp(Item item);
 	void attack();
 	void equipWeapon(Weapon weapon);
-	void unequipWeapon(Weapon weapon);
+	void unequipWeapon();
 	void equipEquipment(Equipment equipment);
-	void unequipEquipment(Equipment equipment);
+	void unequipEquipment();
 }
