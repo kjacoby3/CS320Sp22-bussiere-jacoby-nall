dfc19b108c9da2fe85c276a1c2f03291b3b87970
lnall <99634814+lnall12@users.noreply.github.com>
2022-05-10 22:00:24 -0400

    Password Hashing Implemented

--- TBAG_Project/src/cs320/TBAG/servlet/ajax/CreateAccountAjaxServlet.java
+++ TBAG_Project/src/cs320/TBAG/servlet/ajax/CreateAccountAjaxServlet.java
@@ -1,7 +1,13 @@
 package cs320.TBAG.servlet.ajax;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,54 +30,83 @@ public class CreateAccountAjaxServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException{
+		DerbyDatabase db = new DerbyDatabase();
 		System.out.println("CreateAccountAjaxdoPost");
 		session = req.getSession();
 		System.out.println(req.getParameter("username"));
-		if(req.getParameter("username").equals("")  || req.getParameter("password").equals("") || req.getParameter("confirmPassword").equals("")) {
-			System.out.println("else3");
-			//req.setAttribute("errorMessage", "Enter a Username and your password twice");
+		byte[] salt = new byte[16];
+		String saltHex = null;
+		String passwordHashHex = null;
+		
+		
+		/*try {
+			byte[] salt1 = new byte[16];
+			salt1 = getSalt();
+			System.out.println(toHex(salt1));
+			System.out.println(generateHash("password", salt1));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		
+		if(db.selectPasswordFromUsername(req.getParameter("username"))==null) {
+			try {
+				salt = getSalt();
+				saltHex = toHex(salt);
+				passwordHashHex = generateHash(req.getParameter("password"), salt);
+			} catch (NoSuchAlgorithmException e1) {
+				e1.printStackTrace();
+			} catch (InvalidKeySpecException e) {
+				e.printStackTrace();
+			}
 			
-			//req.getRequestDispatcher("/_view/createAccount.jsp").forward(req, resp);
+			
+			db.insertAccount(req.getParameter("username"), passwordHashHex, saltHex);
 			resp.setContentType("text/plain");
-			resp.getWriter().write("fill");
+			session.setAttribute("playerID", 1);
+			resp.getWriter().write(req.getParameter("username"));
 			resp.getWriter().close();
 		}
-		
-		else if(req.getParameter("username") != null  && req.getParameter("password") != null && req.getParameter("confirmPassword")!= null) {
-			
-			if(req.getParameter("password").equals(req.getParameter("confirmPassword"))) {
-				System.out.println("if");
-				DerbyDatabase db = new DerbyDatabase();
-				db.insertAccount(req.getParameter("username"), req.getParameter("password"));
-				
-				
-				/*session.setAttribute("saveUsername", req.getParameter("username"));
-				session.setAttribute("savePassword", req.getParameter("password"));*/
-				resp.setContentType("text/plain");
-				session.setAttribute("playerID", 1);
-				resp.getWriter().write(req.getParameter("username"));
-				resp.getWriter().close();
-			}
-			else {
-				System.out.println("else1");
-				/*req.setAttribute("errorMessage", "Passwords do not match");
-				req.getRequestDispatcher("/_view/createAccount.jsp").forward(req, resp);*/
-				resp.setContentType("text/plain");
-				resp.getWriter().write("match");
-				resp.getWriter().close();
-			}
-		}
-		
 		else {
-			System.out.println("else2");
-			/*req.setAttribute("errorMessage", "Enter a Username and your password twice");
-			
-			req.getRequestDispatcher("/_view/createAccount.jsp").forward(req, resp);*/
-			resp.setContentType("text/plain");
-			resp.getWriter().write("fill");
+			resp.getWriter().write("exists");
 			resp.getWriter().close();
 		}
 		
+		
+	}
+	
+	private static byte[] getSalt() throws NoSuchAlgorithmException{
+	    SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+	    byte[] salt = new byte[16];
+	    sr.nextBytes(salt);
+	    return salt;
+	}
+	private static String generateHash (String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		int iterations = 1000;
+	    char[] chars = password.toCharArray();
+
+	    PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+	    SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+	    byte[] hash = skf.generateSecret(spec).getEncoded();
+	    return toHex(hash);
+	}
+	
+	private static String toHex(byte[] array) throws NoSuchAlgorithmException{
+	    BigInteger bi = new BigInteger(1, array);
+	    String hex = bi.toString(16);
+	    
+	    int paddingLength = (array.length * 2) - hex.length();
+	    if(paddingLength > 0)
+	    {
+	        return String.format("%0"  +paddingLength + "d", 0) + hex;
+	    }else{
+	        return hex;
+	    }
 	}
 	
 }
